f977312107d5d62f63926b33802511959d54ab85
Joshgit commit -m 'made env files'! <joshiszatt@hotmail.com>
2022-11-16 15:10:09 +0800

    manual

--- manual.sh
+++ manual.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-echo 'You wish to have the power of a golden god?'
+echo 'You wish to have the power of a golden god?' (y/n)
 read answer
 
-if [ "$answer" == 'YES' ]; then
+if [ "$answer" == 'y' ]; then
     echo 'THIS IS NOT A STARTER COMMAND THIS IS A FINISHER COMMAND YOU ARE NOW STUCK IN THIS CONTAINER FOREVER'
-    sudo -u docker docker exec -it $(sudo -u docker docker run -d -v $1:/phanatic/MANUAL \
-        iszatt/phanatic:1.0.0 sleep 1d) bash
+    sudo -u docker docker exec -it $(sudo -u docker docker run -d -v $1:/orange/MANUAL \
+        iszatt/orange:0.0.1 sleep 1d) bash
 fi
 
-if [ "$answer" == 'no' ]; then
+if [ "$answer" == 'n' ]; then
     echo 'Are you not aware of .... the implication?'
     exit 1
 fi
